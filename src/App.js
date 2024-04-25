3734ec995fad22a6f75598925a7cc3b919145186
Ro706 <rohitmandal99091@gmail.com>
2024-04-26 00:50:05 +0530

    update app.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with Ro706
         </a>
       </header>
     </div>
